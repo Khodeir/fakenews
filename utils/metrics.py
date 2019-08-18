2f4bcbda5ede0bfab780f40e3d442ca32eaa0849
Khodeir <m.khodeir@mail.utoronto.ca>
2019-08-17 23:31:39 -0400

    eval + dropout

---
+++ utils/metrics.py
@@ -0,0 +1,12 @@
+from sklearn.metrics import f1_score
+def simple_accuracy(preds, labels):
+    return (preds == labels).mean()
+
+def acc_and_f1(preds, labels):
+    acc = simple_accuracy(preds, labels)
+    f1 = f1_score(y_true=labels, y_pred=preds, average='macro')
+    return {
+        "acc": acc,
+        "f1": f1,
+        "acc_and_f1": (acc + f1) / 2,
+    }
