a061d9552feed815c5dbba641bfa2c56bde4d7a7
BW <bw@BWs-MacBook-Pro.local>
2019-08-29 12:15:27 -0400

    fix position ids dim issue

--- berting/modeling.py
+++ berting/modeling.py
@@ -18,10 +18,18 @@ class BertForMultiSequenceClassification(BertPreTrainedModel):
 
     def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None,
                 position_ids=None, head_mask=None):
+        # input_ids was meant to be (batch_size, max_seq_length)
+        # hack it to be (num_articles, max_seq_length)
+        input_ids = input_ids[0]
+        token_type_ids = token_type_ids[0] if token_type_ids else None
+        attention_mask = attention_mask[0] if attention_mask else None
+        # labels dim is fine as is
+        position_ids = position_ids[0] if position_ids else None
+        head_mask = head_mask[0] if head_mask else None
         outputs = self.bert(input_ids, position_ids=position_ids, token_type_ids=token_type_ids,
                             attention_mask=attention_mask, head_mask=head_mask)
         pooled_outputs = outputs[1]
-        # should be size (batch, num_articles, hidden)
+        # should be size (num_articles, hidden)
         mean = pooled_outputs.mean(dim=-2)
         mean = self.dropout(mean)
         logits = self.classifier(mean)
