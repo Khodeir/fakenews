3a8bedcf17ce44a77fd20f6067e3d651d0f4e9c9
BW <bw@paragraphsword.local>
2019-11-29 13:56:06 -0500

    train on full set

--- berting/data_providers.py
+++ berting/data_providers.py
@@ -66,7 +66,7 @@ class FakeNewsProcessor(DataProcessor):
 
     def get_train_examples(self, data_dir):
         """See base class."""
-        return self._create_examples(data_dir, "train_split_v2_dupd.json")
+        return self._create_examples(data_dir, "train_dupd.json")
 
     def get_dev_examples(self, data_dir):
         """See base class."""
